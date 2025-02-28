cc0f6dfd6f92f294d8d25e118243f2e3bf4a2cae
Idyia <minecraftbutler@gmail.com>
2025-02-28 11:05:51 +0000

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Image_Sorter_DotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
