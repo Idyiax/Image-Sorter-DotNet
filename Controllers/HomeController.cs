9b205b8957f572e18df447e9fc10699de659bc79
Idyia <minecraftbutler@gmail.com>
2025-03-05 19:15:05 +0000

    Created image submission form (non-functional)
    It does show a preview of the image you select tho.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult AddImage()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
