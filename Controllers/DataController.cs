e961833b33c0cd007ef601ce5a194b1999e6d38a
Idyia <minecraftbutler@gmail.com>
2025-03-11 16:09:39 +0000

    Organised data controller and created basic tag adding/getting

--- Controllers/DataController.cs
+++ Controllers/DataController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Image_Sorter_DotNet.Models;
 using Image_Sorter_DotNet.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Drawing;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Image_Sorter_DotNet.Controllers;
 
@@ -17,8 +19,99 @@ public class DataController : ControllerBase
         _context = context;
     }
 
+    #region Gets
+    /// <summary>
+    /// Get the row of a specified image from the Images table.
+    /// </summary>
+    /// <param name="id"> The id of the image. </param>
+    /// <returns> The row of the specified image. </returns>
+    [HttpGet("getimage/{id}")]
+    public async Task<ActionResult<Images>> GetImage(int id)
+    {
+        var image = await _context.Images.FindAsync(id);
+
+        if (image == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(image);
+    }
+    
+    /// <summary>
+    /// Find an image by it's name.
+    /// </summary>
+    /// <param name="name"> The name of the image to find. </param>
+    /// <returns> The row of the specified image. </returns>
+    [HttpGet("findimage/{name}")]
+    public async Task<ActionResult<Images>> FindImage(string name)
+    {
+        var image = await _context.Images.FirstOrDefaultAsync(i => i.FileName == name);
+
+        if (image == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(image);
+    }
+    
+    /// <summary>
+    /// Get the row of a specified tag from the Tags table.
+    /// </summary>
+    /// <param name="id"> The id of the tag. </param>
+    /// <returns> The row of the specified tag. </returns>
+    [HttpGet("gettag/{id}")]
+    public async Task<ActionResult<Tags>> GetTag(int id)
+    {
+        var tag = await _context.Tags.FindAsync(id);
+
+        if (tag == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(tag);
+    }
+
+    /// <summary>
+    /// Find a tag by it's name.
+    /// </summary>
+    /// <param name="name"> The name of the tag to find. </param>
+    /// <returns> The row of the specified tag. </returns>
+    [HttpGet("findtag/{name}")]
+    public async Task<ActionResult<Tags>> FindTag(string name)
+    {
+        var tag = await _context.Tags.FirstOrDefaultAsync(t => t.TagName == name);
+
+        if (tag == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(tag);
+    }
+
+    /// <summary>
+    /// Get all of the rows currently within the Images table.
+    /// </summary>
+    /// <returns> A list of all rows in the Images table. </returns>
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Images>>> GetAllImages()
+    {
+        return await _context.Images.ToListAsync();
+    }
+
+    #endregion Gets
+    #region Posts
+    /// <summary>
+    /// Add an image to the Images table.
+    /// </summary>
+    /// <param name="file"> The image file from a form. </param>
+    /// <param name="name"> The image name from a form. </param>
+    /// <returns> The row of the new image. </returns>
     [HttpPost]
-    public async Task<ActionResult<Images>> PostImage([FromForm] IFormFile file, [FromForm] string? name)
+    public async Task<ActionResult<Images>> AddImage([FromForm] IFormFile file, [FromForm] string? name)
     {
         // Create unique filename
         var uniqueFileName = $"{Guid.NewGuid()}_{file.FileName}";
@@ -62,38 +155,24 @@ public class DataController : ControllerBase
             await _context.SaveChangesAsync();
         }
 
-        catch
+        catch(Exception e)
         {
             // Delete the created image if adding the database entry fails
             System.IO.File.Delete(filePath);
-            return BadRequest();
+            return BadRequest($"Could not add the image {name} because of the following error: {e}");
         }
 
         return CreatedAtAction(nameof(GetImage), new { id = image.Id }, image);
     }
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<Images>> GetImage(int id)
-    {
-        var image = await _context.Images.FindAsync(id);
-
-        if (image == null)
-        {
-            return NotFound();
-        }
-
-        return Ok(image);
-    }
-
-
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<Images>>> GetAllImages()
-    {
-        return await _context.Images.ToListAsync();
-    }
-
+    /// <summary>
+    /// Updates the name of a given image in the Images table.
+    /// </summary>
+    /// <param name="id"> The id of the image to rename. </param>
+    /// <param name="name"> The new name of the image. </param>
+    /// <returns> The new file name. </returns>
     [HttpPost("{id}/{name}")]
-    public async Task<IActionResult> UpdateImageName(int id, string name)
+    public async Task<IActionResult> RenameImage(int id, string name)
     {
         var image = await _context.Images.FindAsync(id);
 
@@ -109,6 +188,11 @@ public class DataController : ControllerBase
         return Ok(image.FileName);
     }
 
+    /// <summary>
+    /// Deletes an image from the Images table.
+    /// </summary>
+    /// <param name="id"> The id of the image to delete. </param>
+    /// <returns></returns>
     [HttpPost("{id}")]
     public async Task<IActionResult> DeleteImage(int id)
     {
@@ -127,4 +211,31 @@ public class DataController : ControllerBase
 
         return Ok();
     }
+
+    [HttpPost("{name}/{colour}")]
+    public async Task<IActionResult> AddTag(string name, Color colour)
+    {
+        var tag = new Tags
+        {
+            TagName = name,
+            ColourR = colour.R,
+            ColourG = colour.G,
+            ColourB = colour.B,
+            CreatedDate = DateTime.UtcNow
+        };
+
+        try
+        {
+            _context.Tags.Add(tag);
+            await _context.SaveChangesAsync();
+        }
+
+        catch(Exception e)
+        {
+            return BadRequest($"Could not add the tag {name} because of the following error: {e}");
+        }
+
+        return CreatedAtAction(nameof(FindTag), new { id = tag.Id }, tag);
+    }
+    #endregion Posts
 }
